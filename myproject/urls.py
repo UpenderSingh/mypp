0698be71cc0c13a9131bbab470953f7ab0806e64
upendersingh <upenders379@gmail.com>
2016-08-27 17:44:05 +0530

    new app created

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
+    url(r'^$', 'myapp.views.hompage', name='hompage'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
